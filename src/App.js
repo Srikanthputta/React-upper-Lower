de870e5a19cbf29496e43f5b33cec040d01e6699
Srikanthputta <61934979+Srikanthputta@users.noreply.github.com>
2022-12-16 00:07:16 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+
+
+      
+
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Welcome to medpro
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn Medpro
+        </a>
+      </header>
+
+     
+
+    </div>
+  );
+}
+
+export default App;
